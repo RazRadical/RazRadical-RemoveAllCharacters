7f727947d672af774100a3ddfea466f88dfd7850
RazaDon Willrich <razradical93@gmail.com>
2025-04-12 01:41:33 +0000

    Completed Remove All Characters.

--- src/main/java/RemoveAllCharacter.java
+++ src/main/java/RemoveAllCharacter.java
@@ -12,6 +12,9 @@ public class RemoveAllCharacter {
      * @return str, without any characters of type ch.
      */
     public String removeAll(String str, String ch){
-        return null;
+
+        String blankString = str.replaceAll(ch, "");
+        
+        return blankString;
     }
 }
